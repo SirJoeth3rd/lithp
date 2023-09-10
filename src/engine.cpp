aa96d59c290d00008b76837335efb5b284a7bd3c
SirJoeth3rd <njdewaal@gmail.com>
2023-09-10 13:55:31 +0200

    added support for first class functions

--- src/engine.cpp
+++ src/engine.cpp
@@ -3,6 +3,10 @@
 
 using std::string;
 
+//TODO, delete later when you use engine header file
+class Engine;
+typedef std::function<lval*(lval*,Engine*)> lithp_func;
+
 struct StrCompare : public std::binary_function<const char*, const char*, bool> {
 public:
     bool operator() (const char* str1, const char* str2) const
@@ -33,7 +37,7 @@ public:
 
   void eval(lval* expr_head) {
     //Note we probaly need to pass a double pointer here
-    bool symbol_exists;
+    bool found;
     while (expr_head) {
       switch (expr_head->type) {
       case Int:
@@ -44,8 +48,8 @@ public:
 	break;
       case Symbol:
 	//Here we do symbol lookup
-	symbol_exists = symbol_table.count(expr_head->data.Symbol);
-	if  (symbol_exists) {
+	found = symbol_table.count(expr_head->data.Symbol);
+	if  (found) {
 	  set_lval(expr_head,fetch_symbol(expr_head->data.Symbol));
 	}
 	break;
@@ -77,16 +81,21 @@ public:
     *x = *y;
   }
 
-  void subscribe_func(
-		      std::function<lval*(lval*, Engine* engine)> func,
-		      const char* name
-		      )
-  {
+  void subscribe_func(lithp_func func,const char* name) {
+    function_table[name] = func;
+  }
+
+  void subscribe_macro(lithp_func func, const char* name) {
     function_table[name] = func;
   }
 
   lval* call_func(const char* name, lval* input) {
     //TODO, check for built in functions here
+    if (!is_func(name)) {
+      //must be a symbol
+      lval* lookup = fetch_symbol(name);
+      name = lookup->data.Symbol;
+    }
     return function_table[name](input, this);
   }
 
@@ -97,13 +106,29 @@ public:
   void set_symbol(const char* name, lval* value) {
     symbol_table[name] = value;
   }
+
+  bool is_func(const char* name) {
+    return function_table.count(name);
+  }
+
+  bool is_macro(const char* name) {
+    return macro_table.count(name);
+  }
+
+  bool is_symbol(const char* name) {
+    return symbol_table.count(name);
+  }
   
 private:
   std::stack<lval*> exprs;
   std::map<
     const char*,
-    std::function<lval*(lval*, Engine* engine)>,
+    lithp_func,
     StrCompare> function_table;
+  std::map<
+    const char*,
+    lithp_func,
+    StrCompare> macro_table;
   std::map<
     const char*,
     lval*,
@@ -144,36 +169,26 @@ lval* set(const lval* head,Engine* engine) {
     //wud?
   }
 
-  engine->set_symbol(head->data.Symbol, head->next);
+  const char* name = head->data.Symbol;
+  engine->set_symbol(name, head->next);
+  
   return nullptr;
 }
 
 int main() {
-  string test = "set(x,2)";
+  string test = "[1,2,[4,5],9]";
 
   std::vector<Token> tokens = tokenize(test);
-  for (Token t : tokens) {
-    if (t.val) {
-      std::cout << t.val <<  ':' << t.Type << '\n';
-    } else {
-      switch (t.Type) {
-      case Token::LBrack:
-	std::cout << '(' << '\n';
-	break;
-      case Token::RBrack:
-	std::cout << ')' << '\n';
-	break;
-      case Token::Comma:
-	std::cout << ',' << '\n';
-	break;
-      }
-    }
-  }
+
+  print_tokens(tokens);
+
 
   std::cout << "==========PARSER TESTS==========" << '\n';
   lval* root = parse_tokens(tokens);
   std::cout << "==========PRINT_AST==========\n";
   print_ast(root);
+
+  /*
 
   std::cout << "==========EVUALATOR TEST========" << '\n';
   Engine engine;
@@ -182,10 +197,11 @@ int main() {
   engine.subscribe_func(set, "set");
 
   
-  engine.parse_push("set(x,2)");
+  engine.parse_push("set(x,plus)");
   engine.eval_top();
-  engine.parse_push("plus(x,3)");
+  engine.parse_push("x(2,3)");
   engine.eval_top();
   lval* top = engine.pop();
   std::cout << top->data.Int << std::endl;
+  */
 }
