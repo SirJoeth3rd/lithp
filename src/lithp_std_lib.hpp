bf10e1804c824175a29fbc5c163b749374895e18
Johann <njdewaal@gmail.com>
2024-11-03 17:06:49 +0200

    the rewrite begins

--- src/lithp_std_lib.hpp
+++
@@ -1,5 +0,0 @@
-//running into circular dependencies without this header
-
-#include "lithp.hpp"
-
-void load_all_functions(lithp_engine*);
---
+++ src/tokenizer.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+typedef enum TokenType_ {
+  lbrack,
+  rbrack,
+  comma,
+  symbol,
+  number,
+  string,
+  end
+} TokenType;
+
+typedef struct Token_ {
+  const char* pos;
+  int length;
+  TokenType toktype;
+} Token;
+
+//GLOBALS
+const int BUFFER = 100;
+Token* TOKENS;
+int TOKIND = 0;
+
+//function definitions
+void tokenize_symbol(void);
+void tokenize_number(void);
+void tokenize_string(void);
+void incchar(void);
+bool isdelimiter(char);
+
+const char* CHR;
+char CHRVAL;
+void incchar(void) {
+  CHR++;
+  CHRVAL = *CHR;
+}
+
+bool isdelimiter(char chr) {
+  switch (chr) {
+  case '[':
+  case ']':
+  case ',':
+    return true;
+  default:
+    return false;
+  }
+}
+
+typedef enum TokenizationError_ {
+  generic_error,
+  multiple_dots_in_number,
+  unexpected_end_of_string,
+  newline_in_string
+} TokenizationError;
+
+bool ERROR = false; 
+void handle_error(TokenizationError error) {
+  printf("got error\n");
+  TOKENS[0] = (Token){"error",5,string};
+  TOKENS[1] = (Token){NULL,0,end};
+  ERROR = true;
+}
+
+Token* tokenize(const char* string) {
+  //init TOKENS
+  TOKENS = malloc(sizeof(Token) * BUFFER);
+  
+  for (CHR = string; CHRVAL; incchar()) {
+    if (isalpha(CHRVAL)) {
+      tokenize_symbol();
+    } else if (isdigit(CHRVAL)) {
+      tokenize_number();
+    } else {
+      switch (CHRVAL) {
+      case '"':
+	tokenize_string();
+	break;
+      case '[':
+	TOKENS[TOKIND] = (Token){NULL,0,lbrack};
+	TOKIND += 1;
+	break;
+      case ']':
+	TOKENS[TOKIND] = (Token){NULL,0,rbrack};
+	TOKIND += 1;
+	break;
+      case ',':
+	TOKENS[TOKIND] = (Token){NULL,0,comma};
+	TOKIND += 1;
+	break;
+      default:
+	handle_error(generic_error);
+      }
+    }
+    if (ERROR) {
+      break;
+    }
+  }
+
+  TOKENS[TOKIND] = (Token){NULL,0,end};
+  
+  return TOKENS;
+}
+
+void tokenize_symbol() {
+  const char* start = CHR;
+  int length = 0;
+  while (CHRVAL) {
+    if (isalnum(CHRVAL)) {
+      length++;
+      incchar();
+    } else if (isspace(CHRVAL) || isdelimiter(CHRVAL)) {
+      TOKENS[TOKIND] = (Token){start,length,symbol};
+      TOKIND += 1;
+    } else {
+      handle_error(generic_error);
+    }
+  }
+  if (!CHRVAL) {
+    handle_error(unexpected_end_of_string);
+  }
+}
+
+void tokenize_number() {
+  const char* start = CHR;
+  int length = 0;
+  bool isfloat = false;
+  while (CHRVAL) {
+    if (isdigit(CHRVAL)) {
+      length++;
+      incchar();
+    }  else if (CHRVAL == '.') {
+      if (isfloat) {
+	handle_error(multiple_dots_in_number);
+	return;
+      } else {
+	isfloat = true;
+	incchar();
+      }
+    } else if (isspace(CHRVAL) || isdelimiter(CHRVAL)) {
+      TOKENS[TOKIND] = (Token){start,length,symbol};
+      TOKIND += 1;
+    } else {
+      handle_error(generic_error);
+    }
+  }
+  if (!CHRVAL) {
+    handle_error(unexpected_end_of_string);
+  }
+}
+
+void tokenize_string() {
+  const char* start = CHR;
+  int length = 0;
+  while (CHRVAL) {
+    if (CHRVAL == '"') {
+      TOKENS[TOKIND] = (Token){start,length,symbol};
+      TOKIND += 1;
+    } else if (CHRVAL == '\n') {
+      handle_error(newline_in_string);
+    }
+    incchar();
+  }
+  if (!CHRVAL) {
+    handle_error(unexpected_end_of_string);
+  }
+}
+
+int main() {
+  const char* expr = "function[alpha,beta]";
+
+  Token* tokens = tokenize(expr);
+
+  int len = 0;
+  for (;(*tokens).toktype != end;tokens++) {
+    len += 1;
+  }
+
+  printf("%i",len);
+}
